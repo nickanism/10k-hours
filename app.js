c058c2553d107f9d71174c6230dc9ff3eae6b2a3
Woo Hyun An <nickanism@gmail.com>
2021-11-25 21:51:54 +0900

    [FIX] adjusted settings for production

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require('cors');
+const path = require('path');
 
 
 // Connect to Database
@@ -13,12 +14,20 @@ app.use(express.json({ extended: false }));
 // CORS
 app.use(cors())
 
-app.get('/', (req, res) => res.send('API Running'));
-
 // Define Routes
 app.use('/api/user', require('./routes/api/user'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/exertion', require('./routes/api/exertion'));
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+  // Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+}
 
 // Init Server
 const PORT = process.env.PORT || 5000;

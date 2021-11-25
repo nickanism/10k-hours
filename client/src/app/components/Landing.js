4b804c690ba1b07235158c4505632980976eee65
Woo Hyun An <nickanism@gmail.com>
2021-11-25 23:13:26 +0900

    [FIX] landing page clause changed

--- client/src/app/components/Landing.js
+++ client/src/app/components/Landing.js
@@ -42,8 +42,8 @@ export const Landing = () => {
 
   return (
     <section className="container">
-      <h1>Landing Page</h1>
-      { isAuthenticated ? <h3>Welcome Back!</h3> : signUpSignIn }
+      <h1>Embark on a journey to mastery</h1>
+      { isAuthenticated ? <h3>Welcome!</h3> : signUpSignIn }
       <img 
         src={corgiStudying} alt="studying" 
       />
